d592762b4d310036949a6664ea9b182a6fcdee16
dulinqiang <377872100@qq.com>
2018-10-10 10:00:57 +0800

    继承

--- test/method.py
+++ test/method.py
@@ -1,5 +1,8 @@
 class test1:
-    def __init__(self):
+    count=0
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
         print("已经生成!!!")
 
     def getname(sele,mess):
@@ -8,5 +11,7 @@ class test1:
 # list=['aab',2,'123','肚里去奶奶个']
 # list1=[2,4]
 # print(list+list1)
+a=test1("dulinqiang",23)
+print(a.name)
 
 
