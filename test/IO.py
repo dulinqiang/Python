c1e12e69b257e838d2d7ba85ef02daec722145c4
dulinqiang <377872100@qq.com>
2018-10-15 14:23:33 +0800

    爬取一阶段

--- test/IO.py
+++ test/IO.py
@@ -1,6 +1,9 @@
-fo=open("C:/Users/Administrator/Desktop/abc.txt",'a+')
-str=fo.read(20)
-print("读入的内容为:",str)
-fo.write("333")
-fo.close()
-print("123")
+# fo=open("C:/Users/Administrator/Desktop/abc.txt",'a+')
+# # str=fo.read(20)
+# # print("读入的内容为:",str)
+# # fo.write("333")
+# # fo.close()
+# # print("123")
+# urls = ['https://www.yrw.com/products/list-direct-all-performance-1-createTimeDesc-{}.html'.format(str(i)) for i in range(1,11)]
+urls= ['i am {}'.format(m) for m in ['kk','ss','jj']]
+print(urls)
--- test/pachong/baidu.py
+++ test/pachong/baidu.py
@@ -1,19 +1,34 @@
 import requests
 import re
 import json
+import xlrd
+import xlwt
+import lxml
+from bs4 import BeautifulSoup
 # try:
 class baidu:
     def __init__(self):
         pass
     def gettitle(self):
+        fo = open("C:/Users/Administrator/Desktop/abc.txt", 'w+')
         try:
-            res=requests.get(url='http://tieba.baidu.com/p/3138733512?see_lz=1&pn=1')
-            rule='<a.*?>(.*?)</a>'
-            # print(res.text)
-            for i in re.findall(rule,res.text):
-                 print(i)
+            res=requests.get(url='http://tieba.com/p/3138733512?see_lz=1&pn=1')
+            rule='<div id="post_content\w*" class="d_post_content j_d_post_content ">(.*?)</div>'
+                 # '[\u4e00-\u9fa5]*[0-9]*[\u4e00-\u9fa5]+'
+            m=BeautifulSoup(res.text,'lxml')
+            # for i in m.descendants:
+            #     print(i.str)
+            print(m.p)
+            # for i in m.find_all('p'):
+            #     print(i)
+            # print(m.p)
+            # fo.write(m.text)
+            # for i in re.findall(rule,res.text):
+            #      print(i)
         except Exception as e:
             print("爬取是出现错误：",e)
+        finally:
+            fo.close()
 t=baidu()
 t.gettitle()
 
